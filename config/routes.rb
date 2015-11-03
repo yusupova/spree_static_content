d4bdd5b9ea608f8971129f5597afd9c6368766bf
Albina Yusupova <yusupova.albina@gmail.com>
2015-11-03 12:15:32 +0500

    change static content module

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Spree::Core::Engine.add_routes do
   namespace :admin do
     resources :pages
+    constraints(Spree::StaticPage) do
+      get '/(*path)', to: 'categories#determine_content', as: 'static'
+    end
   end
 end
--- lib/spree_static_content.rb
+++ lib/spree_static_content.rb
@@ -17,7 +17,7 @@ module Spree
   class StaticPage
     def self.matches?(request)
       return false if request.path =~ %r{\A\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+}
-      !Spree::Page.visible.find_by_slug(request.path).nil?
+      !Spree::Page.visible.find_by_slug(request.path).nil? ^ !Spree::Category.find_by_link(request.path).nil?
     end
   end
 end
