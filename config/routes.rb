57457200f9cdcade48c0bd5d30786808816df6d5
Albina Yusupova <yusupova.albina@gmail.com>
2015-11-03 11:49:54 +0500

    kill static route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Spree::Core::Engine.add_routes do
   namespace :admin do
     resources :pages
   end
-  constraints(Spree::StaticPage) do
-    get '/(*path)', to: 'static_content#show', as: 'static'
-  end
 end
